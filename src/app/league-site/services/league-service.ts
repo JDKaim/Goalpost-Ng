ed61bbeb35098e29849ee9405f3c990719373bc1
JDKaim <jdkaim@hotmail.com>
2024-02-14 22:34:22 -0800

    Fix LeagueService bug

--- src/app/league-site/services/league-service.ts
+++ src/app/league-site/services/league-service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core';
-import { ReplaySubject, interval, map, of, subscribeOn, switchMap, take, tap } from 'rxjs';
+import {
+  ReplaySubject,
+  interval,
+  map,
+  of,
+  subscribeOn,
+  switchMap,
+  take,
+  tap,
+} from 'rxjs';
 import { League } from '../models/league';
 import { Team } from '../models/team';
 
@@ -18,11 +27,8 @@ export class LeagueService {
 
   constructor() {
     this.#league = this.#loadData();
-    if (!this.#league) {
-      this.#resetLeague();
-    }
     this.#subject.next(this.#league);
-    this.watchLeague$().subscribe({next: (league) => this.#saveData(league)});
+    this.watchLeague$().subscribe({ next: (league) => this.#saveData(league) });
     // interval(5000).subscribe({
     //   next: () => {
     //     this.#league.teams.push({ name: 'Washington State', id: '2' });
@@ -36,7 +42,9 @@ export class LeagueService {
   }
 
   watchTeam$(id: string) {
-    return this.watchLeague$().pipe(map(league => league.teams.find((team) => team.id === id)));
+    return this.watchLeague$().pipe(
+      map((league) => league.teams.find((team) => team.id === id))
+    );
   }
 
   #saveData(league: League) {
@@ -46,29 +54,38 @@ export class LeagueService {
     );
   }
 
-  #resetLeague() {
-    this.#saveData({teams: [{ name: 'Washington', id: '0' }, { name: 'Oregon', id: '1' }]});
-    this.#league = this.#loadData();
+  resetLeague() {
+    this.#league = {
+      teams: [
+        { name: 'Washington', id: '0' },
+        { name: 'Oregon', id: '1' },
+      ],
+    };
+    this.#saveData(this.#league);
   }
 
   #loadData() {
-    let json = localStorage.getItem(LeagueService.LeagueStorageKey);
-    if (!json) {
-      this.#saveData(this.#league);
-      json = JSON.stringify(this.#league);
+    const json = localStorage.getItem(LeagueService.LeagueStorageKey);
+    try {
+      return JSON.parse(json!) as League;
+    } catch {
+      this.resetLeague();
+      return this.#league;
     }
-
-    return JSON.parse(json) as League;
   }
 
   createTeam(team: Team) {
     if (!team.id || !team.name) {
-      throw new Error("Team must have ID and name.");
+      throw new Error('Team must have ID and name.');
     }
-    if (this.#league.teams.find(curTeam => curTeam.id === team.id || curTeam.name === team.name)) {
-      throw new Error("Team already exists.");
+    if (
+      this.#league.teams.find(
+        (curTeam) => curTeam.id === team.id || curTeam.name === team.name
+      )
+    ) {
+      throw new Error('Team already exists.');
     }
-    this.#league.teams.push({...team});
+    this.#league.teams.push({ ...team });
     this.#subject.next(this.#league);
     return of(team);
   }

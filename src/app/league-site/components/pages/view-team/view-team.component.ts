c5f76b3e53c48b1cb767796e9cb25c80163293e3
JDKaim <jdkaim@hotmail.com>
2024-02-15 22:24:54 -0800

    Add team pipe and implement in view-team

--- src/app/league-site/components/pages/view-team/view-team.component.ts
+++ src/app/league-site/components/pages/view-team/view-team.component.ts
@@ -7,13 +7,14 @@ import { Observable } from 'rxjs';
 import { Game } from 'src/app/league-site/models/game';
 import { GameStats } from 'src/app/league-site/models/game-stats';
 import { Team } from 'src/app/league-site/models/team';
+import { TeamPipe } from 'src/app/league-site/pipes/team.pipe';
 import { LeagueService } from 'src/app/league-site/services/league-service';
 
 @Component({
   standalone: true,
   selector: 'view-team',
   templateUrl: './view-team.component.html',
-  imports: [CommonModule, RouterModule, CardModule, ButtonModule],
+  imports: [CommonModule, RouterModule, CardModule, ButtonModule, TeamPipe],
 })
 export class ViewTeamComponent implements OnInit {
   #leagueService = inject(LeagueService);
